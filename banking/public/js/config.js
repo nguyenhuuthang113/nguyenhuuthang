4424a9b1815cb9f9a5c60c81ad71fec9fb09f10b
Nguyễn Hữu Thắng <84012140+nguyenhuuthang113@users.noreply.github.com>
2023-08-04 23:12:50 +0700

    upload code 2 year

---
+++ banking/public/js/config.js
@@ -0,0 +1,37 @@
+const WEB = {
+    title: "THÔNG TIN THANH TOÁN",
+    favicon: "logoweb.png",
+};
+
+const USER = {
+    username: "Nguyễn Hữu Thắng",
+    blueTick: false,
+    avatar: "a.jpg",
+    skills: [
+        "DESINGER",
+        "DEVELOPER"
+    ],
+    banks: [
+        {
+            name: "TP BANK",
+            logo: "mbbank.png",
+            number: "0919602479",
+            owner: "Nguyễn Hữu Thắng"
+        },
+        
+        {
+            name: "TP BANK",
+            logo: "mbbank.png",
+            number: "04219836801",
+            owner: "Nguyễn Hữu Thắng"
+        },
+        {
+            name: "MOMO",
+            logo: "momo.png",
+            number: "0919602479",
+            owner: "Nguyễn Hữu Thắng",
+            fastTransfer: true
+        },
+        
+    ]
+};
---
+++ public/js/config.js
@@ -0,0 +1,60 @@
+const __DATA__ = {
+    'config' : {
+        'title' : 'HỮU THẮNG CODER',
+        'favicon' : 'home.png',
+        'notice': '🤗Welcome To Visit My Website😘'
+    },
+    'cover-image' : 'ItsukishimaGateJapan-day.gif',
+    'avatar-image' : 'avatar.jpg',
+    'name' : 'Hữu Thắng', 
+    'is_tick' : true,
+    'skills' : [
+        'Developer',
+        'Designer',
+    ],
+    'bio-storys' : [
+        {
+            'image' : 'admin.png',
+            'content' : 'CONTACT ME👇',
+            'author' : 'Nguyễn Hữu Thắng',
+        },
+    ],
+    'links' : [
+        {
+            'image' : 'wallet.png',
+            'title' : 'Billing Information', 
+            'show-value' : 'Nguyễn Hữu Thắng',
+            'value' : 'https://xn--huthng-sf8b7t.vn/banking',
+            'type' : 0,
+        },
+        {
+            'image' : 'facebook.jpg',
+            'title' : 'My Facebook', 
+            'show-value' : 'Contact Me Via Facebook' ,
+            'value' : 'https://www.facebook.com/100004539734779',
+            'type' : 0,
+        },
+        {
+            'image' : 'zalo.jpg',
+            'title' : 'My Zalo', 
+            'show-value' : 'Contact Me Via Zalo',
+            'value' : 'https://zalo.me/0919602479',
+            'type' : 0,
+        },
+        {
+            'image' : 'phone.jpg',
+            'title' : 'Hotline', 
+            'show-value' : 'Contact Me By Phone Number',
+            'value' : 'tel:0919602479',
+            'type' : 0,
+        },
+        {
+            'image' : 'gmail.jpg',
+            'title' : 'Gmail', 
+            'show-value' : 'Contact Me Via Gmail',
+            'value' : 'mailto:huuthang030603@gmail.com',
+            'type' : 0,
+        },
+    ]
+};
+
